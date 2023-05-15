0f73ed039f1fd696848dc0b6219a39a1624f9a4e
herobruno <125412241+herobruno@users.noreply.github.com>
2023-05-15 16:18:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.get('/info', (req, res) => {
   }
 
   if (carroEncontrado) {
-    res.render('info.ejs', { carro: carroEncontrado, imagemUrl: carroEncontrado.getImagem() });
+    res.render('info', { carro: carroEncontrado, imagemUrl: carroEncontrado.getImagem() });
   } else {
     res.send('Car not found!');
   }
